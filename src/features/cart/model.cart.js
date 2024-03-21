df6f177a61b271d1c088297f61bc1a6ea14bf1d8
Anil Kumar Vind <akumar64587@gmail.com>
2024-03-21 19:58:05 +0530

    change the cart model

--- src/features/cart/model.cart.js
+++ src/features/cart/model.cart.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 const cartSchema = mongoose.Schema({
     productId: {
-        type: mongoose.Schema.Types.ObjectId, // Correct usage of mongoose.Schema.Types.ObjectId
-        ref: 'Product', // Assuming 'Product' is the name of the related model
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Product', 
     },
     quantity: {
         type: Number,
         default: 1,
     },
     userId: {
-        type: mongoose.Schema.Types.ObjectId, // Correct usage of mongoose.Schema.Types.ObjectId
-        ref: 'User', // Assuming 'User' is the name of the related model
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
     }
 });
 
-const Cart = mongoose.model('Cart', cartSchema); // Corrected the schema variable name
+const Cart = mongoose.model('Cart', cartSchema); 
 module.exports = Cart;
